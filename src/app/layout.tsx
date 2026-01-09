73163a0ae7d07a199485a61fcfd9cbd3099beb0d
ez0000001000000 <sjez@einhorn.co>
2026-01-09 15:43:11 -0500

    add-vercel-analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import { Toaster } from "sonner";
+import { Analytics } from "@vercel/analytics/next"
 
 export const metadata: Metadata = {
   title: "Snowbase Studio",
@@ -17,6 +18,7 @@ export default function RootLayout({
       <body className="antialiased">
         {children}
         <Toaster position="bottom-right" closeButton />
+        <Analytics />
       </body>
     </html>
   );
